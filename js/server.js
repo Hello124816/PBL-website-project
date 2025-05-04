052db64a3bbdb669d43f2b731dbcd5e6cd6681ce
USER <USER@Mac.Dlink>
2025-05-04 09:35:48 +0800

    V17

---
+++ js/server.js
@@ -0,0 +1,35 @@
+// server.js
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// MySQL connection
+const connection = mysql.createConnection({
+  host: "rare-disease-med-database.c7qai64ma8pc.ap-southeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Babybaby0720",
+  database: "RareDisease" // Connect directly to RareDisease
+});
+
+// Test database connection
+connection.connect(error =>{
+    if (error){console.log("There's an error")}
+    
+    else{
+        console.log("There's no error")
+    }
+        
+})
+app.post('/search', (req, res) => {
+  
+});
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
